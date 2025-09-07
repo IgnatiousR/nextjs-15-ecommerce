5bb3d9959bb5069bbb986cf362fd106c78a73795
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-07 21:08:32 +0600

    Add product list com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import ProductList from "@/components/product-list";
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="">Homepage</div>;
+  return (
+    <div className="">
+      <div className="relative aspect-[3/1] mb-10">
+        <Image src="/featured.png" alt="Banner Image" fill />
+      </div>
+      <ProductList />
+    </div>
+  );
 }
