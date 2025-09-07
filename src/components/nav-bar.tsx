c9ba094e6f5bdf28affbe0578368be54240f2049
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-07 16:04:54 +0600

    Create nav-bar.tsx

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { ModeToggle } from './mode-toggle';
+import SearchBar from './search-bar';
+import { Bell, Home, ShoppingCart } from 'lucide-react';
+
+const NavBar = () => {
+  return (
+    <nav className="w-full flex items-center justify-between  border-b border-neutral-200 pb-4">
+      {/* Left */}
+      <Link href="/" className="flex items-center">
+        <Image src="/logo.png" alt="Logo" width={36} height={36} className="w-6 h-6 md:w-9 md:h-9"></Image>
+        <p className="hidden md:block text-md font-semibold  uppercase">Nxt Store</p>
+      </Link>
+
+      {/* Right */}
+      <div className="flex gap-6 items-center">
+        <SearchBar />
+        <Link href="/">
+          <Home className="w-4 h-4" />
+        </Link>
+        <Bell className="w-4 h-4" />
+        <ShoppingCart className="w-4 h-4" />
+        <ModeToggle />
+        <Link href="login">Sign in</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
