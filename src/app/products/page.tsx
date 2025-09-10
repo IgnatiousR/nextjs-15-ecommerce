498937383c3a6677513fceb331c44bbd4be8c4ff
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-10 22:07:44 +0600

    Created Products

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import ProductList from "@/components/product-list";
+
+export default async function Products({ searchParams }: { searchParams: Promise<{ category: string }> }) {
+  const category = (await searchParams).category;
+  return (
+    <div className="mt-10">
+      <ProductList category={category} params="products" />
+    </div>
+  );
+}
