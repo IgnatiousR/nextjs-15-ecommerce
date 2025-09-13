3dd438110ef5797347c33d6b3535a83bd82df615
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-13 14:30:26 +0600

    Create cart-items-data.ts

---
+++ src/data/cart-items-data.ts
@@ -0,0 +1,65 @@
+import { CartItemsType } from "../../types";
+
+export const cartItems: CartItemsType = [
+  {
+    id: 1,
+    name: "Adidas CoreFit T-Shirt",
+    shortDescription: "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 39.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: [
+      { name: "neutral", hex: "#808080" },
+      { name: "green", hex: "#008000" },
+      { name: "purple", hex: "#800080" },
+    ],
+    images: {
+      neutral: "/products/1g.png",
+      purple: "/products/1p.png",
+      green: "/products/1gr.png",
+    },
+    quantity: 1,
+    selectedSize: "m",
+    selectedColor: "neutral",
+  },
+  {
+    id: 2,
+    name: "Puma Ultra Warm Zip",
+    shortDescription: "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 59.9,
+    sizes: ["s", "m", "l", "xl"],
+    colors: [
+      { name: "neutral", hex: "#808080" },
+      { name: "green", hex: "#008000" },
+    ],
+    images: { neutral: "/products/2g.png", green: "/products/2gr.png" },
+    quantity: 1,
+    selectedSize: "l",
+    selectedColor: "neutral",
+  },
+  {
+    id: 3,
+    name: "Nike Air Essentials Pullover",
+    shortDescription: "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 69.9,
+    sizes: ["s", "m", "l"],
+    colors: [
+      { name: "green", hex: "#008000" },
+      { name: "blue", hex: "#000080" },
+      { name: "black", hex: "#00000000" },
+    ],
+    images: {
+      green: "/products/3gr.png",
+      blue: "/products/3b.png",
+      black: "/products/3bl.png",
+    },
+    quantity: 1,
+    selectedSize: "l",
+    selectedColor: "black",
+  },
+];
