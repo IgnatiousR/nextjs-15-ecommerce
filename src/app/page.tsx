f9a20b4167a75f60227371f766807f7810c0e058
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-15 16:23:54 +0600

    added banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
+import Banner from "@/components/banner";
 import ProductList from "@/components/product-list";
 import Image from "next/image";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
 
 export default async function HomePage({
   searchParams,
@@ -19,32 +13,7 @@ export default async function HomePage({
       {/* <div className="relative aspect-[3/1] mb-10">
         <Image src="/featured.png" alt="Banner Image" fill />
       </div> */}
-      <Carousel
-        className="mb-10"
-        opts={{
-          loop: true,
-        }}
-      >
-        <CarouselContent>
-          <CarouselItem>
-            <div className="relative aspect-[3/1]">
-              <Image src="/featured.png" alt="Banner 1" fill className="object-cover rounded-xl" />
-            </div>
-          </CarouselItem>
-          <CarouselItem>
-            <div className="relative aspect-[3/1]">
-              <Image
-                src="/featured-0.png"
-                alt="Banner 2"
-                fill
-                className="object-cover rounded-xl"
-              />
-            </div>
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+      <Banner />
       <ProductList category={category} params="homepage" />
     </div>
   );
---
+++ src/components/banner.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef, useState, useEffect } from "react";
+
+const Banner = () => {
+  const autoplay = useRef(
+    Autoplay({
+      delay: 2500,
+      stopOnInteraction: false,
+    })
+  );
+
+  const [api, setApi] = useState<any>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) return;
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap());
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
+
+  return (
+    <div>
+      <Carousel
+        className="mb-4"
+        opts={{
+          loop: true,
+        }}
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+        setApi={setApi} // capture Embla API
+      >
+        <CarouselContent>
+          <CarouselItem>
+            <div className="relative aspect-[3/1]">
+              <Image src="/featured.png" alt="Banner 1" fill className="object-cover rounded-xl" />
+            </div>
+          </CarouselItem>
+          <CarouselItem>
+            <div className="relative aspect-[3/1]">
+              <Image
+                src="/featured-0.png"
+                alt="Banner 2"
+                fill
+                className="object-cover rounded-xl"
+              />
+            </div>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      {/* Pagination Dots */}
+      <div className="flex justify-center gap-2 mt-2 mb-4">
+        {Array.from({ length: count }).map((_, i) => (
+          <button
+            key={i}
+            onClick={() => api?.scrollTo(i)}
+            className={`h-2 w-2 rounded-full transition-all ${
+              current === i ? "bg-neutral-700 w-4" : "bg-neutral-400"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
