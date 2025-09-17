03ffba170199b4534a8b4760d5b8e1d50b420de0
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-17 22:08:52 +0600

    updated

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -6,6 +6,7 @@ import { products } from "@/data/products-data";
 import ProductInteraction from "@/components/product-interaction";
 
 export const generateMetadata = async ({ params }: { params: Promise<{ id: string }> }) => {
+  //Get from db
   const { id } = await params;
   const product = products.find((p) => p.id === parseInt(id)) as ProductType;
 
@@ -32,23 +33,14 @@ const ProductPage = async ({
     <div className="flex flex-col gap-4 lg:flex-row md:gap-12 mt-12">
       {/* IMAGE */}
       <div className="w-full lg:w-5/12 relative aspect-[2/3]">
-        <Image
-          src={product.images[selectedColor]}
-          alt={product.name}
-          fill
-          className="object-contain rounded-md"
-        />
+        <Image src={product.images[selectedColor]} alt={product.name} fill className="object-contain rounded-md" />
       </div>
       {/* DETAILS */}
       <div className="w-full lg:w-7/12 flex flex-col gap-4">
         <h1 className="text-2xl font-medium">{product.name}</h1>
         <p className="text-neutral-500">{product.description}</p>
         <h2 className="text-2xl font-semibold">${product.price.toFixed(2)}</h2>
-        <ProductInteraction
-          product={product}
-          selectedSize={selectedSize}
-          selectedColor={selectedColor}
-        />
+        <ProductInteraction product={product} selectedSize={selectedSize} selectedColor={selectedColor} />
         {/* CARD INFO */}
         <div className="flex items-center gap-2 mt-4">
           <Image src="/klarna.png" alt="klarna" width={50} height={25} className="rounded-md" />
@@ -56,11 +48,10 @@ const ProductPage = async ({
           <Image src="/stripe.png" alt="stripe" width={50} height={25} className="rounded-md" />
         </div>
         <p className="text-neutral-500 text-xs">
-          By clicking Pay Now, you agree to our{" "}
-          <span className="underline hover:text-black">Terms & Conditions</span> and{" "}
-          <span className="underline hover:text-black">Privacy Policy</span>. You authorize us to
-          charge your selected payment method for the total amount shown. All sales are subject to
-          our return and <span className="underline hover:text-black">Refund Policies</span>.
+          By clicking Pay Now, you agree to our <span className="underline hover:text-black">Terms & Conditions</span>{" "}
+          and <span className="underline hover:text-black">Privacy Policy</span>. You authorize us to charge your
+          selected payment method for the total amount shown. All sales are subject to our return and{" "}
+          <span className="underline hover:text-black">Refund Policies</span>.
         </p>
       </div>
     </div>
